3f246eda9dcaa78cb48030e670d6127a29236208
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-04-09 18:25:18 +0530

    add test for all reducers conditions

--- src/reducers/reducer.test.js
+++ src/reducers/reducer.test.js
@@ -3,7 +3,13 @@ import reducer from "./reducer";
 
 test('should return default initial state of "null" when no action is passed', () => {
   const newState = reducer(undefined, {});
-  expect(newState).toStrictEqual({"error": false, "page": 0, "page_size": 0, "todos": [], "todos_list": []});
+  expect(newState).toStrictEqual({
+    error: false,
+    page: 0,
+    page_size: 0,
+    todos: [],
+    todos_list: [],
+  });
 });
 
 test('should return state of array of todos upon receiving an action of type "SET_TODOS"', () => {
@@ -17,6 +23,66 @@ test('should return state of array of todos upon receiving an action of type "SE
     page: 0,
     page_size: 0,
     todos: [{ userId: 1, id: 1, title: "delectus aut autem" }],
+    todos_list: [],
+  });
+});
+
+test('should return state of page upon receiving an action of type "SET_PAGE"', () => {
+  const newState = reducer(undefined, {
+    type: actionTypes.SET_PAGE,
+    payload: 2,
+  });
+
+  expect(newState).toStrictEqual({
+    error: false,
+    page: 2,
+    page_size: 0,
+    todos: [],
+    todos_list: [],
+  });
+});
+
+test('should return state of page_size upon receiving an action of type "SET_PAGE_SIZE"', () => {
+  const newState = reducer(undefined, {
+    type: actionTypes.SET_PAGE_SIZE,
+    payload: 1,
+  });
+
+  expect(newState).toStrictEqual({
+    error: false,
+    page: 0,
+    page_size: 1,
+    todos: [],
+    todos_list: [],
+  });
+});
+
+test('should return state of array of todos_list upon receiving an action of type "SET_TODOS_LIST"', () => {
+  const newState = reducer(undefined, {
+    type: actionTypes.SET_TODOS_LIST,
+    payload: [{ userId: 1, id: 1, title: "delectus aut autem" }],
+  });
+
+  expect(newState).toStrictEqual({
+    error: false,
+    page: 0,
+    page_size: 0,
+    todos: [],
+    todos_list: [{ userId: 1, id: 1, title: "delectus aut autem" }],
+  });
+});
+
+test('should return state of error upon receiving an action of type "SET_ERROR"', () => {
+  const newState = reducer(undefined, {
+    type: actionTypes.SET_ERROR,
+    payload: true,
+  });
+
+  expect(newState).toStrictEqual({
+    error: true,
+    page: 0,
+    page_size: 0,
+    todos: [],
     todos_list: [],
   });
 });
